d7ce4f02c24adc8a791e68ccc01612f6a30843f5
russa <aaron.russ@dfki.de>
2019-02-22 22:43:28 +0100

    added meta-data

---
+++ config.d.ts
@@ -0,0 +1,51 @@
+
+import { MediaManagerPluginEntry } from 'mmir-lib';
+
+/**
+ * (optional) entry "asrAnroid" and "ttsAndroid" in main configuration.json
+ * for settings of asrAndroid and ttsAndroid module.
+ *
+ * Some of these settings can also be specified by using the options argument
+ * in the ASR and TTS functions of {@link PluginMediaManager}, e.g.
+ * {@link PluginMediaManager#recognize} or {@link PluginMediaManager#startRecord}
+ * (if specified via the options, values will override configuration settings).
+ */
+export interface PluginConfig {
+  asrAndroid?: ASRPluginConfigEntry;
+  ttsAndroid?: TTSPluginConfigEntry | PluginSpeechConfigEntry;
+}
+
+
+export interface ASRPluginConfigEntry extends MediaManagerPluginEntry {
+
+  //TODO?
+  // /** OPTIONAL number of n-best results that should (max.) be returned: integer, DEFAULT 1 */
+  // results?: number;
+
+  //TODO?
+  // /** OPTIONAL  set recognition mode */
+  // mode?: 'search' | 'dictation';
+  
+  //TODO support credentials via JS?
+}
+
+export interface TTSPluginConfigEntry extends MediaManagerPluginEntry {
+  //TODO support credentials via JS?
+}
+
+/**
+ * Speech config entry for the plugin: per language (code) configuration e.g. for
+ * adjusting the language-code or setting a specific voice for the language
+ */
+export interface PluginSpeechConfigEntry extends SpeechConfigPluginEntry {
+  /** OPTIONAL
+   * the language/country for TTS
+   * @type string
+   */
+  language?: string;
+  /** OPTIONAL
+   * a specific voice for TTS
+   * @type string
+   */
+  voice?: 'female' | 'male' | string;
+}
