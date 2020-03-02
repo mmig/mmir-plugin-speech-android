1cd81273aa3f050d1a737daf6ac7f1ecaeb191e7
russa <aaron.russ@dfki.de>
2020-03-02 12:46:01 +0100

    generate stub-functions instead of explicitly defining individual functions

--- www/alt/stub/androidasr_stub.js
+++ www/alt/stub/androidasr_stub.js
@@ -1,35 +1,18 @@
-﻿/*
- * 	Copyright (C) 2012-2015 DFKI GmbH
- * 	Deutsches Forschungszentrum fuer Kuenstliche Intelligenz
- * 	German Research Center for Artificial Intelligence
- * 	http://www.dfki.de
- * 
- * 	Permission is hereby granted, free of charge, to any person obtaining a 
- * 	copy of this software and associated documentation files (the 
- * 	"Software"), to deal in the Software without restriction, including 
- * 	without limitation the rights to use, copy, modify, merge, publish, 
- * 	distribute, sublicense, and/or sell copies of the Software, and to 
- * 	permit persons to whom the Software is furnished to do so, subject to 
- * 	the following conditions:
- * 
- * 	The above copyright notice and this permission notice shall be included 
- * 	in all copies or substantial portions of the Software.
- * 
- * 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- * 	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- * 	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * 	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
- * 	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- * 	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- * 	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
+var subFuncs2nd = ['stopRecord', 'cancel', 'getLanguages'];
+var subFuncs3rd = ['recognize', 'recognizeNoEOS', 'startRecord', 'stopRecord'];
 
-
-var exec = require('cordova/exec');
+function createStubs(func, errorCallbackIndex){
+	return function(){
+		var errCb = arguments[errorCallbackIndex];
+		if(errCb){
+			errCb('Error for '+func+'(): not implemented');
+		}
+	};
+}
 
 /**
- *  
+ *
  * @return Instance of AndroidASRPlugin
  */
 var AndroidASRPlugin = function() {
@@ -37,111 +20,22 @@ var AndroidASRPlugin = function() {
 	this.__micListener = [];
 };
 
-AndroidASRPlugin.prototype.recognize = function(language, successCallback, failureCallback, withIntermediateResults){
-//	 return exec(successCallback,
-//   					 failureCallback,
-//   					 'AndroidASRPlugin',
-//   					 'recognize',
-//   					 [language, withIntermediateResults? true : false]);
-	if(failureCallback){
-		failureCallback('Error for recognize(): not implemented');
-	}
-};
+subFuncs2nd.map(function(name){
+	AndroidASRPlugin.prototype[name] = createStubs(name, 2);
+});
+
+subFuncs3rd.map(function(name){
+	AndroidASRPlugin.prototype[name] = createStubs(name, 3);
+});
 
 /**
- * @deprecated use #startRecord instead
+ *
+ * @return Instance of AndroidASRPlugin
  */
-AndroidASRPlugin.prototype.recognizeNoEOS = function(language, successCallback, failureCallback, withIntermediateResults){
-//	this.startRecord(language, successCallback, failureCallback, withIntermediateResults);
-	if(failureCallback){
-		failureCallback('Error for recognizeNoEOS(): not implemented');
-	}
+var AndroidASRPlugin = function() {
+	//list of listeners for the "microphone levels changed" event
+	this.__micListener = [];
 };
-
-AndroidASRPlugin.prototype.startRecord = function(language, successCallback, failureCallback, withIntermediateResults){
-	
-//	return exec(successCallback,
-//					 failureCallback,
-//					 'AndroidASRPlugin',
-//					 'startRecording',
-//					 [language, withIntermediateResults? true : false]);
-	if(failureCallback){
-		failureCallback('Error for startRecord(): not implemented');
-	}
-};
-
-AndroidASRPlugin.prototype.stopRecord = function(successCallback, failureCallback){
-
-//	 return exec(successCallback,
-// 					 failureCallback,
-// 					 'AndroidASRPlugin',
-// 					 'stopRecording',
-// 					 []);
-	if(failureCallback){
-		failureCallback('Error for stopRecord(): not implemented');
-	}
-};
-
-AndroidASRPlugin.prototype.cancel = function(successCallback, failureCallback){
-
-//	 return exec(successCallback,
-//   					 failureCallback,
-//   					 'AndroidASRPlugin',
-//   					 'cancel',
-//   					 []);
-
-	if(failureCallback){
-		failureCallback('Error for cancel(): not implemented');
-	}
-};
-
-AndroidASRPlugin.prototype.getLanguages = function(successCallback, failureCallback){
-
-//	 return exec(successCallback,
-//   					 failureCallback,
-//   					 'AndroidASRPlugin',
-//   					 'getSupportedLanguages',
-//   					 []);
-
-		if(failureCallback){
-			failureCallback('Error for getLanguages(): not implemented');
-		}
-};
-
-///**
-// * Get the microphone levels ("recording levels") when recording (i.e. when voice recognition is active).
-// * 
-// * @EXPERIMENTAL
-// * 
-// * @param successCallback
-// * 			callback function which takes one parameter ARRAY:
-// * 			An Array of Float values from range [0,90] that were gathered since
-// *             the recording was started / the last call of getMicLevels()
-// * @param failureCallback
-// */
-//AndroidASRPlugin.prototype.getMicLevels = function(successCallback, failureCallback){
-//
-//	 return exec(successCallback,
-//  					 failureCallback,
-//  					 'AndroidASRPlugin',
-//  					 'getMicLevels',
-//  					 []);
-//};
-
-/**
- * Functions for listening to the microphone levels
- * 
- * register a handler:
- * 	onMicLevelChanged(listener: Function)
- * 
- * remove a handler:
- *  offMicLevelChanged(listener: Function)
- *  
- * get the list of all currently registered listeners
- *  getMicLevelChangedListeners() : Array[Function]
- * 
- * @EXPERIMENTAL
- */
 
 AndroidASRPlugin.prototype.fireMicLevelChanged = function(value){
 	for(var i=0, size = this.__micListener.length; i < size; ++i){
@@ -150,18 +44,8 @@ AndroidASRPlugin.prototype.fireMicLevelChanged = function(value){
 };
 
 AndroidASRPlugin.prototype.onMicLevelChanged = function(listener){
-	var isStart = this.__micListener.length === 0; 
+	var isStart = this.__micListener.length === 0;
 	this.__micListener.push(listener);
-	
-//	if(isStart){
-//		//start the RMS-changed processing (i.e. fire change-events for microphone-level changed events
-//		return exec(function(){console.info('AndroidASRPlugin: started processing microphone-levels');},
-//				 function(err){console.error('AndroidASRPlugin: Error on start processing microphone-levels! ' + err);},
-//				 'AndroidASRPlugin',
-//				 'setMicLevelsListener',
-//				 [true]
-//		);
-//	}
 };
 
 AndroidASRPlugin.prototype.getMicLevelChangedListeners = function(){
@@ -175,32 +59,13 @@ AndroidASRPlugin.prototype.offMicLevelChanged = function(listener){
 	if(size){
 		for(var i = size - 1; i >= 0; --i){
 			if(this.__micListener[i] ===  listener){
-				
-				//move all handlers after i by 1 index-position ahead:
-				for(var j = size - 1; j > i; --j){
-					this.__micListener[j-1] = this.__micListener[j];
-				}
-				//remove last array-element
-				this.__micListener.splice(size-1, 1);
-				
+				this.__micListener.splice(i, 1);
 				isRemoved = true;
 				break;
 			}
 		}
 	}
-	
-//	if(isRemoved && this.__micListener.length === 0){
-//		//stop RMS-changed processing (no handlers are listening any more!)
-//		return exec(function(){console.info('AndroidASRPlugin: stopped processing microphone-levels');},
-//				 function(err){console.error('AndroidASRPlugin: Error on stop processing microphone-levels! ' + err);},
-//				 'AndroidASRPlugin',
-//				 'setMicLevelsListener',
-//				 [false]
-//		);
-//	}
-	
 	return isRemoved;
 };
 
 module.exports = new AndroidASRPlugin();
-
--- www/alt/stub/androidtts_stub.js
+++ www/alt/stub/androidtts_stub.js
@@ -1,30 +1,15 @@
-﻿/*
- * 	Copyright (C) 2012-2015 DFKI GmbH
- * 	Deutsches Forschungszentrum fuer Kuenstliche Intelligenz
- * 	German Research Center for Artificial Intelligence
- * 	http://www.dfki.de
- * 
- * 	Permission is hereby granted, free of charge, to any person obtaining a 
- * 	copy of this software and associated documentation files (the 
- * 	"Software"), to deal in the Software without restriction, including 
- * 	without limitation the rights to use, copy, modify, merge, publish, 
- * 	distribute, sublicense, and/or sell copies of the Software, and to 
- * 	permit persons to whom the Software is furnished to do so, subject to 
- * 	the following conditions:
- * 
- * 	The above copyright notice and this permission notice shall be included 
- * 	in all copies or substantial portions of the Software.
- * 
- * 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- * 	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- * 	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * 	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
- * 	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- * 	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- * 	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
-var exec = require('cordova/exec');
+var subFuncs2nd = ['startup', 'shutdown', 'getLanguage', 'getVoice', 'getLanguages', 'getVoices', 'getDefaultLanguage', 'getDefaultVoice', 'cancel'];
+var subFuncs3rd = ['speak', 'silence', 'isLanguageAvailable', 'setLanguage', 'setVoice'];
+
+function createStubs(func, errorCallbackIndex){
+	return function(){
+		var errCb = arguments[errorCallbackIndex];
+		if(errCb){
+			errCb('Error for '+func+'(): not implemented');
+		}
+	};
+}
 
 /**
  */
@@ -35,241 +20,14 @@ AndroidTTSPlugin.STOPPED = 0;
 AndroidTTSPlugin.INITIALIZING = 1;
 AndroidTTSPlugin.STARTED = 2;
 
-/**
- * Play the passed in text as synthesized speech
- * @function speak 
- * @param {DOMString} text
- * @param {DOMString} language
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.tts = function(text, language, successCallback, errorCallback) {
-	
-//     return exec(successCallback, errorCallback, "AndroidTTSPlugin", "speak", [text, language]);
-	if(errorCallback){
-		errorCallback('Error for tts(): not implemented');
-	}
-};
+AndroidTTSPlugin.prototype.tts = createStubs('tts', 4);
 
-/**
- * @deprecated use #tts function instead (NOTE the different order of the arguments!)
- */
-AndroidTTSPlugin.prototype.speak = function(text, successCallback, errorCallback, language) {
-//    return this.tts(text, language, successCallback, errorCallback);
-	if(errorCallback){
-		errorCallback('Error for speak(): not implemented');
-	}
-};
+subFuncs2nd.map(function(name){
+	AndroidTTSPlugin.prototype[name] = createStubs(name, 2);
+});
 
-/** 
- * Play silence for the number of ms passed in as duration
- * 
- * @function silence 
- * @param {long} duration
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.silence = function(duration, successCallback, errorCallback) {
-//     return exec(successCallback, errorCallback, "AndroidTTSPlugin", "silence", [duration]);
-	if(errorCallback){
-		errorCallback('Error for silence(): not implemented');
-	}
-};
-
-/**
- * Starts up the AndroidTTSPlugin Service
- * 
- * @function startup 
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.startup = function(successCallback, errorCallback) {
-	
-//     return exec(successCallback, errorCallback, "AndroidTTSPlugin", "startup", []);
-	if(errorCallback){
-		errorCallback('Error for startup(): not implemented');
-	}
-};
-
-/**
- * Shuts down the AndroidTTSPlugin Service if you no longer need it.
- * 
- * @function shutdown 
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.shutdown = function(successCallback, errorCallback) {
-//     return exec(successCallback, errorCallback, "AndroidTTSPlugin", "shutdown", []);
-	if(errorCallback){
-		errorCallback('Error for shutdown(): not implemented');
-	}
-};
-
-/**
- * Finds out if the language is currently supported by the AndroidTTSPlugin service.
- * 
- * @function isLanguageAvailable 
- * @param {DOMSting} lang
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.isLanguageAvailable = function(lang, successCallback, errorCallback) {
-//     return exec(successCallback, errorCallback, "AndroidTTSPlugin", "isLanguageAvailable", [lang]);
-	if(errorCallback){
-		errorCallback('Error for isLanguageAvailable(): not implemented');
-	}
-};
-
-/**
- * Finds out the current language of the AndroidTTSPlugin service.
- * 
- * @function successCallback 
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.getLanguage = function(successCallback, errorCallback) {
-//     return exec(successCallback, errorCallback, "AndroidTTSPlugin", "getLanguage", []);
-     if(errorCallback){
- 		errorCallback('Error for getLanguage(): not implemented');
- 	}
-};
-
-/**
- * Finds out the current voice of the AndroidTTSPlugin service.
- * 
- * @function getVoice 
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.getVoice = function(successCallback, errorCallback) {
-//    return exec(successCallback, errorCallback, "AndroidTTSPlugin", "getVoice", []);
-	if(errorCallback){
- 		errorCallback('Error for getVoice(): not implemented');
- 	}
-};
-
-/**
- * Sets the language of the AndroidTTSPlugin service.
- * 
- * @function setLanguage 
- * @param {String} lang
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.setLanguage = function(lang, successCallback, errorCallback) {
-//    return exec(successCallback, errorCallback, "AndroidTTSPlugin", "setLanguage", [lang]);
-	if(errorCallback){
- 		errorCallback('Error for setLanguage(): not implemented');
- 	}
-};
-
-/**
- * Sets the voice of the AndroidTTSPlugin service.
- * 
- * @function setVoice 
- * @param {String} voice
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.setVoice = function(voice, successCallback, errorCallback) {
-//    return exec(successCallback, errorCallback, "AndroidTTSPlugin", "setVoice", [voice]);
-	if(errorCallback){
- 		errorCallback('Error for setVoice(): not implemented');
- 	}
-};
-
-
-/**
- * Get all available languages of the AndroidTTSPlugin service:
- * <code>successCallback(languageList: Array<string>)</code>
- *
- * @function getLanguages
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.getLanguages = function(successCallback, errorCallback) {
-//    return exec(successCallback, errorCallback, "AndroidTTSPlugin", "languageList", []);
-	if(errorCallback){
- 		errorCallback('Error for getLanguages(): not implemented');
- 	}
-};
-
-/**
- * Get all available voices of the AndroidTTSPlugin service:
- * <code>successCallback(voiceList: Array<string>)</code>
- *
- * @function getVoices
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.getVoices = function(successCallback, errorCallback) {
-
-	if(typeof language === 'function'){
-		errorCallback = includeDetails;
-		successCallback = language;
-		language = '';
-		includeDetails = false;
-	} else if(typeof includeDetails === 'function'){
-		errorCallback = successCallback;
-		successCallback = includeDetails;
-		if(typeof language === 'boolean'){
-			includeDetails = language;	
-			language = '';
-		} else {
-			includeDetails = false;
-		}
-	}
-//	var args = [language, includeDetails];
-//	
-//    return exec(successCallback, errorCallback, "AndroidTTSPlugin", "voiceList", args);
-	if(errorCallback){
- 		errorCallback('Error for getVoices(): not implemented');
- 	}
-};
-
-/**
- * Get the default language of the AndroidTTSPlugin service:
- * <code>successCallback(language: string)</code>
- *
- * @function getDefaultLanguage
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.getDefaultLanguage = function(successCallback, errorCallback) {
-//    return exec(successCallback, errorCallback, "AndroidTTSPlugin", "defaultLanguage", []);
-	if(errorCallback){
- 		errorCallback('Error for getDefaultLanguage(): not implemented');
- 	}
-};
-
-/**
- * Get the default voice of the AndroidTTSPlugin service:
- * <code>successCallback(voice: string)</code>
- *
- * @function getDefaultVoice
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.getDefaultVoice = function(successCallback, errorCallback) {
-//    return exec(successCallback, errorCallback, "AndroidTTSPlugin", "defaultVoice", []);
-	if(errorCallback){
- 		errorCallback('Error for getDefaultVoice(): not implemented');
- 	}
-};
-
-
-/**
- * Cancel AndroidTTSPlugin TTS (if active; do nothing if not active).
- * 
- * @function cancel
- * @param {Object} successCallback
- * @param {Object} errorCallback
- */
-AndroidTTSPlugin.prototype.cancel = function(successCallback, errorCallback) {
-//     return exec(successCallback, errorCallback, "AndroidTTSPlugin", "cancel", []);
-	if(errorCallback){
-		errorCallback('Error for cancel(): not implemented');
-	}
-};
+subFuncs3rd.map(function(name){
+	AndroidTTSPlugin.prototype[name] = createStubs(name, 3);
+});
 
 module.exports = new AndroidTTSPlugin();
