47c2b996290593811431d2e5ba87a3edf32c7778
russa <aaron.russ@dfki.de>
2024-02-16 20:15:57 +0100

    migrate android support libraries to androidx namespace

--- src/android/de/dfki/iui/mmir/plugins/speech/android/Utils.java
+++ src/android/de/dfki/iui/mmir/plugins/speech/android/Utils.java
@@ -8,8 +8,8 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.speech.SpeechRecognizer;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 public class Utils {
 
